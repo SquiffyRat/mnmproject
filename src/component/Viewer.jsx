ded624180718a789f771bc6bae47f6b7de635f4f
LSY <peteral@naver.com>
2023-09-14 16:15:18 +0900

    few more comments

---
+++ src/component/Viewer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// 각 calculator에 제공할 컴포넌트
+function Viewer() {
+  return (
+    <div>
+      {/* input */}
+      {/*result screen */}
+      {/* two of accodian button */}
+    </div>
+  );
+}
+
+export default Viewer;
--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -8,7 +8,15 @@ function HomePage() {
 
   return (
     <div>
-      <Header title={homeTitle} summary={'홈페이지 설명'} />
+      <Header
+        title={homeTitle}
+        summary={
+          '미터법 간의 단위변환과 야드파운드법 간의 변환을 보여주는 앱입니다. 야드파운드법은 다시 제국 단위계(Imperial)와 미국 단위계(US)로 나뉘며 이는 북미, 영국, 미얀마 등지에서 쓰입니다.'
+        }
+      />
+      <div>
+        {/* 현재 summary 내용의 일부를 홈페이지 중앙에 위치하도록 수정 */}
+      </div>
       {/* Buttonx3 */}
       {/* current exchange tab */}
     </div>
